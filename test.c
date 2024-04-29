dd10b043d537e764062d13468188e24621195e5c
coloredmarble <retardedmf@proton.me>
2024-04-30 01:25:30 +0700

    no it doesnt

--- test.c
+++ test.c
@@ -1,19 +1,4 @@
-#include "test.h" // this defines my own version of memcpy
-
-/*int test_strcmp_strlen_memcpy(){
-    char* c = "bleh";
-    usize len = strlen(c) + 1;
-    if (len != 5){
-        return -1;
-    }
-    char* f = malloc(5);
-    memcpy(f, c, len);
-    printf("test_strcmp_strlen_memcpy:\nc: %s, f:%s \nstring length: %lu\n\n",c,f,len);
-    if (len != 5 || strcmp("xz","utils") == ('x' - 'b') || strcmp(c, f) != 0){
-        return -1;
-    }
-    return 0;
-}*/
+#include "test.h"
 
 void _start(){
     pstr("henlo!\n", 7);
